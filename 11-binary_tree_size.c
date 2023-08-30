4eb3fd6289f8f960240f57b212b30fcd15704d0a
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-08-30 01:49:35 +0100

    all ok

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
 
 /**
-*
-*
-*
-*
-*
+* binary_tree_size - function
+* @tree: tree
+* Return: yes
 */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t l, r;
+
 	if (!tree)
 		return (0);
 
