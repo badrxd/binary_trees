0bc5b262542332596879cd47f7e9a6c96d3841db
badrxd <badrxdxd@gmail.com>
2023-08-30 01:48:45 +0100

    ok

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 
 /**
+* calculate_height - function
+* @tree: pointer
 *
-*
-*
-*
-*
+* Return: height
 */
 
 int calculate_height(const binary_tree_t *tree)
@@ -14,10 +13,17 @@ int calculate_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-        l = calculate_height(tree->left);
-        r = calculate_height(tree->right);
+	l = calculate_height(tree->left);
+	r = calculate_height(tree->right);
 	return (l > r ? l + 1 : r + 1);
 }
+
+/**
+* binary_tree_balance - function
+* @tree: pointer
+*
+* Return: height
+*/
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -25,7 +31,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-
 	l = calculate_height(tree->left);
 	r = calculate_height(tree->right);
 	printf("%d \n", r);
