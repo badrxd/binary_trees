decdaec4256eb70fe97149f3aae262c57a08c650
badrxd <badrxdxd@gmail.com>
2023-08-30 06:46:40 +0100

    ok

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,13 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-
+	if (node->parent->left && node->parent->right)
+	{
 	if (node->parent->left->n == node->n)
 		return (node->parent->right);
 	else
 		return (node->parent->left);
+	}
+	else
+		return (NULL);
 }
