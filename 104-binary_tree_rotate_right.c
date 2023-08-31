bc54cf87b67053e27f9fd91fe763ba491751fae0
badrxd <badrxdxd@gmail.com>
2023-08-31 22:28:46 +0100

    ok

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_rotate_right - function
+* @tree: pointer
 *
-*
-*
-*
+* Return: root
 */
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 
 	root = tree->left;
 	tree->left = root->right;
+	if (root->right)
+		root->right->parent = tree;
 	root->right = tree;
 	root->parent = tree->parent;
 	tree->parent = root;
